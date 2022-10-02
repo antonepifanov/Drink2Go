6498f5b835c533237379d3037de65f1925a33b86
antonepifanov <antonepifanov87@gmail.com>
2022-10-02 12:16:42 +0300

    Дорабатывает рэндж

--- source/js/modules/init-range.js
+++ source/js/modules/init-range.js
@@ -2,8 +2,8 @@ import noUiSlider from "nouislider";
 
 (()=>{
   const range = document.querySelector(".filter__range-slider");
-  const minPriceInput = document.querySelector(".filter__range-input--min input");
-  const maxPriceInput = document.querySelector(".filter__range-input--max input");
+  const minPriceInput = document.querySelector(".filter__range-input--min input:not([disabled])");
+  const maxPriceInput = document.querySelector(".filter__range-input--max input:not([disabled])");
 
   noUiSlider.create(range, {
     start: [0, 900],
@@ -34,10 +34,30 @@ import noUiSlider from "nouislider";
   range.noUiSlider.on('update', (values, handle) => {
     const value = values[handle];
 
-    handle
-      ? maxPriceInput.value = value
-      : value == 0
-        ? minPriceInput.value = ""
-        : minPriceInput.value = Math.round(value);
+    if (handle) {
+      if(maxPriceInput) {
+        maxPriceInput.value = value;
+      };
+    } else {
+      if (minPriceInput) {
+        if (value == 0) {
+          minPriceInput.value = "";
+        } else {
+          minPriceInput.value = Math.round(value);
+        };
+      };
+    };
   });
+
+  if (minPriceInput) {
+    minPriceInput.addEventListener('change', function () {
+      range.noUiSlider.set([this.value, null]);
+    });
+  };
+
+  if (maxPriceInput) {
+    maxPriceInput.addEventListener('change', function () {
+      range.noUiSlider.set([null, this.value]);
+    });
+  };
 })();
